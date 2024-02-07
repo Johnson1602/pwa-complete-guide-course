3440390d2c8ede3258542964300e2c0cf2616f2f
Weiyi Xu <johnsonxwy@gmail.com>
2024-02-07 17:19:19 +0800

    feat: add promise & fetch test data

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -3,3 +3,33 @@ if ('serviceWorker' in navigator) {
     console.log('=== Service worker registered!')
   })
 }
+
+// const promise = new Promise((resolve) => {
+//   setTimeout(() => {
+//     console.log('=== Promise resolved!')
+//     resolve('success')
+//   }, 1000)
+// })
+
+// promise.then((result) => {
+//   console.log('=== result: ', result)
+// })
+
+// fetch('https://httpbin.org/uuid')
+//   .then((res) => {
+//     console.log('=== res: ', res)
+//     return res.json()
+//   })
+//   .then((data) => {
+//     console.log('=== data: ', data)
+//   })
+
+// fetch('https://httpbin.org/put', {
+//   method: 'PUT',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify({ person: { name: 'John Doe', age: 30 } }),
+// })
+//   .then((res) => res.json())
+//   .then((data) => console.log('=== data: ', data))
