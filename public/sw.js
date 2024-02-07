807812b3c54cc6da37df90db779ed3def00f53fc
Weiyi Xu <johnsonxwy@gmail.com>
2024-02-07 11:35:05 +0800

    feat: init project

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', (e) => {
+  console.log('=== Service worker installed!', e)
+})
+
+self.addEventListener('activate', (e) => {
+  console.log('=== Service worker activated!', e)
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (e) => {
+  console.log('=== Service worker Fetching something!', e.request.url)
+  e.respondWith(fetch(e.request))
+})
