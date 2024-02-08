a51d36c609bc3430d728608e05d0cea95a308d79
Weiyi Xu <johnsonxwy@gmail.com>
2024-02-08 15:57:33 +0800

    feat: service worker 增加 cache

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -8,7 +8,7 @@ if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/sw.js')
     .then(function () {
-      console.log('=== Service worker registered!')
+      // console.log('=== Service worker registered!')
     })
     .catch(function (err) {
       console.log(err)
@@ -16,7 +16,7 @@ if ('serviceWorker' in navigator) {
 }
 
 window.addEventListener('beforeinstallprompt', function (event) {
-  console.log('beforeinstallprompt fired')
+  // console.log('beforeinstallprompt fired')
   event.preventDefault()
   deferredPrompt = event
   return false
--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,64 @@
+/**
+ * cspell:ignore precaching
+ */
+
+const STATIC_CACHE_NAME = 'static-v2'
+const DYNAMIC_CACHE_NAME = 'dynamic-v2'
+
 self.addEventListener('install', (e) => {
-  console.log('=== Service worker installed!', e)
+  // console.log('=== Service worker installed!', e)
+  e.waitUntil(
+    caches.open(STATIC_CACHE_NAME).then((cache) => {
+      // console.log('=== precaching...')
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/src/js/app.js',
+        '/src/js/feed.js',
+        '/src/js/promise.js',
+        '/src/js/fetch.js',
+        '/src/js/material.min.js',
+        '/src/css/app.css',
+        '/src/css/feed.css',
+        '/src/images/main-image.jpg',
+        'https://fonts.googleapis.com/css?family=Roboto:400,700',
+        'https://fonts.googleapis.com/icon?family=Material+Icons',
+        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+      ])
+    })
+  )
 })
 
 self.addEventListener('activate', (e) => {
-  console.log('=== Service worker activated!', e)
-  self.clients.claim()
+  // console.log('=== Service worker activated!', e)
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== STATIC_CACHE_NAME && key !== DYNAMIC_CACHE_NAME) {
+            // console.log('=== deleting key: ', key)
+            return caches.delete(key)
+          }
+        })
+      )
+    })
+  )
+
+  return self.clients.claim()
 })
 
 self.addEventListener('fetch', (e) => {
   // console.log('=== Service worker Fetching something!', e.request.url)
-  e.respondWith(fetch(e.request))
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      if (res) return res
+
+      return fetch(e.request).then((res) => {
+        return caches.open(DYNAMIC_CACHE_NAME).then((cache) => {
+          cache.put(e.request.url, res.clone())
+          return res
+        })
+      })
+    })
+  )
 })
